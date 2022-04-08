14021d441cde903cb48ca4c864a7f1a823c1e373
JiangYancheng <3190102563@zju.edu.cn>
2022-04-08 23:12:02 +0800

    增加tflite模型预测代码

---
+++ HanziRecognition/tflite_predict.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+
+def tflite_predict(model, img):
+    qizi = ['dilei', 'gongbin', 'junqi', 'junzhang', 'lianzhang', 'lvzhang',
+        'paizhang', 'shizhang', 'siling', 'tuanzhang', 'yinzhang', 'zhadan']
+
+    model.allocate_tensors()
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
+
+    img = img.astype(np.float32)
+    input_data = img[np.newaxis, :, :, np.newaxis]
+
+    index = input_details[0]['index']
+    model.set_tensor(index, input_data)
+
+    model.invoke() # 预测
+
+    output_data = model.get_tensor(output_details[0]['index']) # 得到预测结果
+
+    pre_class = np.argmax(output_data, axis=-1)
+    pre_result = qizi[int(pre_class)]
+    return pre_result
+
+if __name__ == '__main__':
+    tflite_model_path = "./results/temp.tflite"
+    img_path = "./data/test/junqi/junqi_0_127.jpg"
+    
+    model = tf.lite.Interpreter(model_path = tflite_model_path) # Load TFLite model
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+    pre_result = tflite_predict(model, img)
+
+    print(pre_result)
+
+    cv2.waitKey(0)
