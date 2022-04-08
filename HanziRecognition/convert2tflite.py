0ae4c7423b4bfa141f323a4e2d8f9417566d82d6
JiangYancheng <3190102563@zju.edu.cn>
2022-04-08 23:11:35 +0800

    增加模型压缩

--- HanziRecognition/convert2tflite.py
+++ HanziRecognition/convert2tflite.py
@@ -8,8 +8,7 @@ tflite_model_path = h5_model_path[:-2] + "tflite"
 h5_model = load_model(h5_model_path)
 converter = tf.lite.TFLiteConverter.from_keras_model(h5_model)
 
-# 开启动态量化
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.optimizations = [tf.lite.Optimize.DEFAULT] # 开启动态量化
 
 tflite_model = converter.convert()
 open(tflite_model_path, "wb").write(tflite_model)
