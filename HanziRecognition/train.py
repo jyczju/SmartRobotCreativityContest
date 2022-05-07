bba8c4df469c57f92762b7fa2bbc4006446cb4c8
JiangYancheng <3190102563@zju.edu.cn>
2022-05-07 23:56:59 +0800

    改善网络结构

--- HanziRecognition/train.py
+++ HanziRecognition/train.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-'''
-训练数据
-author:Administrator
-datetime:2018/3/24/024 19:52
-software: PyCharm
-'''
 from keras.utils.vis_utils import plot_model 
+# from keras.applications.vgg16 import VGG16
 
 from statistics import mode
 from keras.preprocessing.image import ImageDataGenerator
@@ -15,11 +8,12 @@ from tensorflow.keras.models import load_model
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization, SpatialDropout2D
 from tensorflow.keras.optimizers import SGD,Adam
-from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau,TensorBoard
 
 from f1_score import F1_Score
+import datetime
 
 qizi = ['dilei','gongbin','junqi','junzhang','lianzhang','lvzhang','paizhang','shizhang','siling','tuanzhang','yinzhang','zhadan']
  
@@ -34,7 +28,7 @@ save_model_path = "./results/temp.h5"  # 保存模型路径和名称
 # 图片生成器ImageDataGenerator
 train_pic_gen = ImageDataGenerator(
     rescale=1. / 255,  # 对输入图片进行归一化到0-1区间
-    # rotation_range=5,
+    rotation_range= 2,
     # width_shift_range=0.1,
     # height_shift_range=0.1,
     )
@@ -65,42 +59,81 @@ validation_flow = validation_pic_gen.flow_from_directory(
     class_mode='categorical'
 )
 
+# model = Sequential([
+#     Conv2D(filters=64, kernel_size=9, padding='same', activation='relu', input_shape=input_shape),
+#     # SpatialDropout2D(0.2), # 丢弃特征图
+#     BatchNormalization(axis=3), # 批量归一化
+#     MaxPooling2D(pool_size=2),
+
+#     Conv2D(filters=32, kernel_size=9, padding='same', activation='relu'),
+#     # SpatialDropout2D(0.2),
+#     BatchNormalization(axis=3),
+#     MaxPooling2D(pool_size=2),
+
+#     Conv2D(filters=16, kernel_size=5, padding='same', activation='relu'),
+#     # SpatialDropout2D(0.2),
+#     BatchNormalization(axis=3),
+#     MaxPooling2D(pool_size=2),
+
+#     Flatten(),
+
+#     Dense(256, activation='relu'),
+#     BatchNormalization(),
+#     # Dropout(0.4),
+
+#     Dense(32, activation='relu'),
+#     BatchNormalization(),
+#     # Dropout(0.4),
+
+#     Dense(12, activation='softmax') # 必须用softmax
+# ])
+
+
+# ref to VGG16
 model = Sequential([
-    Conv2D(filters=64, kernel_size=9, padding='same', activation='relu', input_shape=input_shape),
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=input_shape),
+    # SpatialDropout2D(0.2),
+    # BatchNormalization(axis=3),
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+    # SpatialDropout2D(0.2),
+    # BatchNormalization(axis=3),
     MaxPooling2D(pool_size=2),
 
-    Conv2D(filters=32, kernel_size=9, padding='same', activation='relu'),
+    Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+    # SpatialDropout2D(0.2),
+    # BatchNormalization(axis=3),
     MaxPooling2D(pool_size=2),
 
-    # Conv2D(filters=32, kernel_size=5, padding='same', activation='relu', input_shape=input_shape),
-    # MaxPooling2D(pool_size=2),
-
-    # Conv2D(filters=64, kernel_size=5, padding='same', activation='relu'),
-    # MaxPooling2D(pool_size=2),
-
-    # Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
-    # MaxPooling2D(pool_size=2),
-
-    Conv2D(filters=16, kernel_size=5, padding='same', activation='relu'),
+    Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
     MaxPooling2D(pool_size=2),
 
     Flatten(),
 
-    Dense(256, activation='relu'),
+    Dense(4096, activation='relu'),
+    # BatchNormalization(),
+    Dropout(0.4),
 
-    # Dense(128, activation='relu'),
-
-    Dense(32, activation='relu'),
+    Dense(1000, activation='relu'),
+    # BatchNormalization(),
+    # Dropout(0.4),
 
     Dense(12, activation='softmax') # 必须用softmax
 ])
 
 # f1_score
 f1_score = F1_Score()
- 
-# sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=1e-5), metrics=['accuracy']) # lr = 1e-5
+
+# 设置训练参数
+print('length of train_flow:', len(train_flow))
+print('length of validation_flow:', len(validation_flow))
+nb_train_samples = int(len(train_flow)/9) # int(len(train_flow)/32) # 50 # 数据多，可以调大
+nb_validation_samples = int(len(validation_flow)/9) # 20 # 数据多，可以调大
+nb_epoch = 80 # 训练轮数
+
+
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True) # decay = 1e-6
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=1e-5), metrics=['accuracy']) # lr = 1e-5
 
 model.summary()
 
@@ -108,7 +141,7 @@ plot_model(model, to_file='model-CNN.png', show_shapes=True)
 
 lr_reduce = ReduceLROnPlateau(monitor='val_accuracy',factor=0.1, patience=3,verbose=1,mode = 'max', min_lr=1e-11)
 
-early_stop = EarlyStopping(monitor='val_accuracy',mode ='max', patience=9, verbose=1)
+early_stop = EarlyStopping(monitor='val_accuracy',mode ='max', patience=12, verbose=1)
 # early_stop = EarlyStopping(monitor='val_f1',mode ='max', patience=12, verbose=1)
 # early_stop = EarlyStopping(monitor='val_loss',mode ='min', patience=9, verbose=1)
 
@@ -116,13 +149,8 @@ early_stop = EarlyStopping(monitor='val_accuracy',mode ='max', patience=9, verbo
 # checkpointer = ModelCheckpoint(filepath="./tmp/weights.hdf5", verbose=1, save_best_only=True)
 checkpointer = ModelCheckpoint(filepath=save_model_path, monitor='val_accuracy',verbose=2,save_best_only=True,save_weights_only=False,mode='auto')
 
-# 设置训练参数
-print('length of train_flow:', len(train_flow))
-print('length of validation_flow:', len(validation_flow))
-nb_train_samples = int(len(train_flow)/4) # int(len(train_flow)/32) # 50 # 数据多，可以调大
-nb_validation_samples = int(len(validation_flow)/4) # 20 # 数据多，可以调大
-nb_epoch = 80 # 训练轮数
-
+# log_dir="./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 # 数据流训练API
 history = model.fit(
